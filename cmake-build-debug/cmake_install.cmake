a0eb63dc86e5cf2ce209604ffe4966adcea5a135
PBBx0 <ravv.original@gmail.com>
2023-03-29 21:32:09 +0300

    cleared some trash

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/pbb/programs/aesc_homework/planar-graphs-project
+# Install script for directory: /home/pbb/programs/aesc_homework/Planarity_Testing
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/pbb/programs/aesc_homework/planar-graphs-project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/pbb/programs/aesc_homework/Planarity_Testing/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
